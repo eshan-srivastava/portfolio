c53c914264ea8f7def4d541deac5d00f340a6888
Eshan Srivastava <eshan,srivastava273@gmail.com>
2024-01-26 22:56:10 +0530

    fixed footer bg not showing in build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer footer-center py-5 relative border-t border-t-gray-500/80 text-base-content rounded mt-8 bg-cover bg-no-repeat bg-center bg-fixed"
         style={{
-            backgroundImage: `linear-gradient(to bottom, rgba(19, 17, 21, 0.2), rgba(13, 17, 21, 0.9)), url('./src/assets/footer_bg.svg')`,
+            backgroundImage: `linear-gradient(to bottom, rgba(19, 17, 21, 0.2), rgba(13, 17, 21, 0.9)), url(/footer_bg.svg)`,
         }}>
             <p className="py-4">
                 Made with React + TailwindCSS + Typescript
