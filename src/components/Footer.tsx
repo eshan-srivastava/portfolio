754a0cd873a278dc2252d30f3d35a6cb443dd4c4
Eshan Srivastava <eshan,srivastava273@gmail.com>
2024-01-26 22:07:19 +0530

    added image to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {   
     return (
-        <footer className="footer footer-center p-10 bg-gray-900 border-t border-t-gray-500/80 text-base-content rounded mt-8">
-            <p className="pb-4">
+        <footer className="footer footer-center py-5 relative border-t border-t-gray-500/80 text-base-content rounded mt-8 bg-cover bg-no-repeat bg-center bg-fixed"
+        style={{
+            backgroundImage: `linear-gradient(to bottom, rgba(19, 17, 21, 0.2), rgba(13, 17, 21, 0.9)), url('./src/assets/footer_bg.svg')`,
+        }}>
+            <p className="py-4">
                 Made with React + TailwindCSS + Typescript
                 <br /><i>by Eshan Srivastava</i>
             </p>
@@ -16,7 +19,7 @@ const Footer = () => {
                 <a href="https://simpleicons.org/" target="_blank" rel="noopener noreferrer" className="link link-hover">simpleicons.org</a>
                 <span> | </span>
                 <a href="https://uxwing.com" target="_blank" rel="noopener noreferrer" className="link link-hover">uxwing.com</a>
-            </aside>
+            </aside>     
         </footer>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'footer': "url('./src/assets/footer_bg.svg')",
+      },
       animation: {
         'gradient' : 'gradient 8s linear infinite',
       },
